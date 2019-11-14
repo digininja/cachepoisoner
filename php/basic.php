a9423102f6b14e845987c411aed5e11fe636d791
Robin Wood <robin@digi.ninja>
2019-11-14 21:21:09 +0000

    Update basic.php
    X-Forwarded-For can be a comma separated string, not just one host.

--- php/basic.php
+++ php/basic.php
@@ -2,8 +2,15 @@
 $host = $_SERVER['HTTP_HOST'];
 
 if (array_key_exists ("HTTP_X_FORWARDED_HOST", $_SERVER)) {
-	$host = $_SERVER['HTTP_X_FORWARDED_HOST'];
+    $forwarded = explode (",", $_SERVER['HTTP_X_FORWARDED_HOST']);
+    var_dump ($forwarded);
+    if (is_array ($forwarded)) {
+        $host = array_shift ($forwarded);
+    } else {
+        $host = $_SERVER['HTTP_X_FORWARDED_HOST'];
+    }   
 }
+
 ?>
 <meta property="og:image" content="http://<?=$host?>/cms/social.png" />
 
