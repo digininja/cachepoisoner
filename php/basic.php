1d39580af222ae853ac06bb52628d33a64f1927f
Robin Wood <robin@digi.ninja>
2019-11-26 18:49:08 +0000

    basic done

--- php/basic.php
+++ php/basic.php
@@ -3,7 +3,6 @@ $host = $_SERVER['HTTP_HOST'];
 
 if (array_key_exists ("HTTP_X_FORWARDED_HOST", $_SERVER)) {
     $forwarded = explode (",", $_SERVER['HTTP_X_FORWARDED_HOST']);
-    var_dump ($forwarded);
     if (is_array ($forwarded)) {
         $host = array_shift ($forwarded);
     } else {
@@ -12,8 +11,24 @@ if (array_key_exists ("HTTP_X_FORWARDED_HOST", $_SERVER)) {
 }
 
 ?>
-<meta property="og:image" content="http://<?=$host?>/cms/social.png" />
-
-<?php
-//var_dump ($_SERVER);
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>Basic Poisoning Lab</title>
+	<meta property="og:title" content="Basic Poisoning Lab" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="http://<?=$host?>/cms/social.png" />
+</head>
+<body>
+	<h1>Basic Poisoning Lab</h1>
+	<p>
+		This page loads its open graph image based on the host requested but isn't that clever when working out the host to use.
+	</p>
+	<p>
+		Use this weakness to poison every visitor with a nice bit of Cross-Site Scripting.
+	</p>
+	<p>
+		<a href="/">&laquo; Back</a>
+	</p>
+</body>
+</html>
