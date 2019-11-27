0c18929cb48fa783fa0d8665392002ae2fc652ba
Robin Wood <robin@digi.ninja>
2019-11-27 22:13:39 +0000

    trying to get redirects working

--- backend/redirect.php
+++ backend/redirect.php
@@ -1,27 +1,39 @@
 <?php
 // var_dump ($_SERVER);
 
-$redirect = "https://digi.ninja";
+// Tell the cache to cache differently for
+// each different user agent string
+
+header ("Vary: User-Agent");
+
+$host = $_SERVER['HTTP_HOST'];
 
 if (array_key_exists ("HTTP_X_FORWARDED_HOST", $_SERVER)) {
-	$redirect = "https://" . $_SERVER['HTTP_X_FORWARDED_HOST'];
+    $forwarded = explode (",", $_SERVER['HTTP_X_FORWARDED_HOST']);
+    if (is_array ($forwarded)) {
+        $host = array_shift ($forwarded);
+    } else {
+        $host = $_SERVER['HTTP_X_FORWARDED_HOST'];
+    }   
 }
 
-function clean_display_string ($str) {
-	return htmlentities ($str);
+if (array_key_exists ("HTTP_USER_AGENT", $_SERVER) && strpos (strtolower ($_SERVER['HTTP_USER_AGENT']), "chrome") !== false) {
+	$redirect = "http://" . $host . "/chrome.php";
+} else {
+	$redirect = "http://" . $host . "/others.php";
 }
 
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">    
 <head>      
 <title>The Tudors</title>      
-<meta http-equiv="refresh" content="0;URL='<?=clean_display_string ($redirect)?>'" />    
+<meta http-equiv="xrefresh" content="4;URL='<?=htmlentities ($redirect)?>'" />    
 </head>    
 <body> 
 <h1>Redirection Poisoning</h1>
 <p>A random number is: <?=mt_rand();?></p>
-<p>This page has moved to a <a href="<?=clean_display_string ($redirect)?>">
-<?=clean_display_string ($redirect)?></a>.</p> 
+<p>This page has moved to a <a href="<?=htmlentities ($redirect)?>">
+<?=htmlentities ($redirect)?></a>.</p> 
 <img src="/images/poison.png" alt="Bottle of poison" />
 </body>  
 </html>     
